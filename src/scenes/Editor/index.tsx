74f88f0efb851ba01483fbf593dae30dad981608
Mustafo Faiz <m.fayzov1@gmail.com>
2020-07-18 04:06:43 +0500

    ✨Typescript parser has done (#3)

--- src/scenes/Editor/index.tsx
+++ src/scenes/Editor/index.tsx
@@ -1,10 +1,18 @@
 import * as React from 'react';
 import MonacoEditor from 'react-monaco-editor';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+import * as ts from 'typescript';
 
 export const Editor: React.FC = () => {
     const [code, setCode] = React.useState('// type your code...');
+
     const options = {
         selectOnLineNumbers: true,
+        roundedSelection: false,
+        readOnly: false,
+        // cursorStyle: 'line',
+        automaticLayout: false,
+
     };
 
     const editorDidMount = (editor: any, monaco: any) => {
@@ -14,12 +22,17 @@ export const Editor: React.FC = () => {
 
     const onChange = (newValue: any, e: any) => {
         console.log('onChange', newValue, e);
+        const { outputText, diagnostics } = ts.transpileModule(newValue, {
+            compilerOptions: { module: ts.ModuleKind.CommonJS },
+        });
+
+        console.log(eval(outputText));
     };
     return (
         <MonacoEditor
             width="800"
             height="600"
-            language="javascript"
+            language="typescript"
             theme="vs-dark"
             value={code}
             options={options}
