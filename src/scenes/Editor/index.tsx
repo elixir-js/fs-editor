ec915ee40afc6643999c53ffa3b80c218c2ce268
Mustafo Faiz <m.fayzov1@gmail.com>
2020-07-18 16:12:16 +0500

    ✨Created console UI where you can log whatever you want. Works only within textarea. (#5)

--- src/scenes/Editor/index.tsx
+++ src/scenes/Editor/index.tsx
@@ -1,10 +1,24 @@
 import * as React from 'react';
 import MonacoEditor from 'react-monaco-editor';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
+const Console = require('console-emitter');
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 import * as ts from 'typescript';
 
 export const Editor: React.FC = () => {
     const [code, setCode] = React.useState('// type your code...');
+    const console = new Console();
+    const consoleEl = React.useRef<HTMLDivElement>(null);
+
+    React.useEffect(() => {
+        const logme = (...args: any) => {
+            global.console.log(args);
+            consoleEl.current!.innerText += `\n${args[0] || ''}`;
+        };
+
+        console.on('log', logme);
+    }, []);
 
     const options = {
         selectOnLineNumbers: true,
@@ -12,32 +26,40 @@ export const Editor: React.FC = () => {
         readOnly: false,
         // cursorStyle: 'line',
         automaticLayout: false,
-
     };
 
     const editorDidMount = (editor: any, monaco: any) => {
-        console.log('editorDidMount', editor);
         editor.focus();
     };
 
     const onChange = (newValue: any, e: any) => {
-        console.log('onChange', newValue, e);
         const { outputText, diagnostics } = ts.transpileModule(newValue, {
             compilerOptions: { module: ts.ModuleKind.CommonJS },
         });
 
-        console.log(eval(outputText));
+        global.console.log(newValue);
+        // setHistory([]);
+        consoleEl.current!.innerHTML = '';
+
+        try {
+            eval(outputText);
+        } catch (e) {
+            consoleEl.current!.innerText = e.toString();
+        }
     };
     return (
-        <MonacoEditor
-            width="800"
-            height="600"
-            language="typescript"
-            theme="vs-dark"
-            value={code}
-            options={options}
-            onChange={onChange}
-            editorDidMount={editorDidMount}
-        />
+        <>
+            <MonacoEditor
+                width="800"
+                height="600"
+                language="typescript"
+                theme="vs-dark"
+                value={code}
+                options={options}
+                onChange={onChange}
+                editorDidMount={editorDidMount}
+            />
+            <div ref={consoleEl}>Your console</div>
+        </>
     );
 };
