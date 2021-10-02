7a9f3c82b0816c078937d6a34f25149968531ef7
fayzzzm <m.fayzov1@gmail.com>
2021-10-02 17:00:35 +0500

    Added feature of handling error in javascript.

--- src/server/files/internal.js
+++ src/server/files/internal.js
@@ -2,3 +2,9 @@ var parent = window.parent;
 console.log = (...args) => {
     parent.postMessage(args, 'http://localhost:9000');
 };
+
+window.onerror = function (msg) {
+    console.log(msg);
+
+    return false;
+};
--- src/server/files/main.js
+++ src/server/files/main.js
@@ -2,3 +2,9 @@ console.log(1);
 
 console.log(1 + 2);
 console.log("Something should be written here in order to be read");
+
+console.log(1 + 3);
+
+console.log(x);
+
+let x = 0;
