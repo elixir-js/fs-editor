1ddb5ffdb083d3d4afe2e502d8faea68285fb00c
fayzzzm <m.fayzov1@gmail.com>
2021-10-03 21:14:31 +0500

    Remove function duplication.

--- src/server/src/routers/file-io/providers/read-file.ts
+++ src/server/src/routers/file-io/providers/read-file.ts
@@ -2,20 +2,7 @@ import { Request, Response } from 'express';
 import * as fs from 'fs';
 import * as path from 'path';
 
-const getFileNameByExtension = (extension: string): string => {
-    switch (extension) {
-        case 'javascript':
-            return 'main.js';
-        case 'html':
-            return 'index.html';
-        case 'css':
-            return 'style.css';
-        default:
-            throw new Error(
-                `This server doesn't support this extension: "${extension}". Please be sure that you are providing extensions such as: "html", "css" or "javascript"!`,
-            );
-    }
-};
+import { getFileNameByExtension } from '@utils/getFileName';
 
 export const readFile = (req: Request, res: Response) => {
     const { extension } = req.params;
