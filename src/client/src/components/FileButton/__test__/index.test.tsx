94573b73bf39b8ef19fd4956df1b3adf6112ab9a
fayzzzm <m.fayzov1@gmail.com>
2021-10-09 23:12:09 +0500

    Provided full test for FileButton component

--- src/client/src/components/FileButton/__test__/index.test.tsx
+++ src/client/src/components/FileButton/__test__/index.test.tsx
@@ -1,17 +1,17 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
+import { Provider } from 'mobx-react';
+import sinon from 'sinon';
+
 import { FileButton } from '..';
 
-import { Provider } from 'mobx-react';
 import * as appModels from '@app/models';
 import { ExtensionType } from '@app/types';
 
 test('Should render properly', () => {
     const props = {
-        handleClick: () => {
-            console.log('Clicked');
-        },
         extension: ExtensionType.JS,
         name: 'main',
     };
@@ -23,10 +23,34 @@ test('Should render properly', () => {
     );
 
     expect(container.firstChild).toHaveClass('file-button');
+});
 
-    // fireEvent.click(container.firstChild);
-    // expect(setActiveIndex).toBeCalledTimes(1);
+test('Should get an error with wrong prop type', () => {
+    const stub = sinon.stub(console, 'error');
 
-    // fireEvent.click(container.lastChild);
-    // expect(setActiveIndex).toBeCalledTimes(2);
+    // @ts-ignore
+    const RenderErrorComponent = <FileButton />;
+    render(<Provider {...appModels}>{RenderErrorComponent}</Provider>);
+
+    expect(stub.called).toBe(true);
+    expect(
+        stub.calledWithExactly(`Warning: Failed prop type: The prop \`name\` is marked as required in \`inject-with-editorDataModel(Function)\`, but its value is \`undefined\`.
+    in inject-with-editorDataModel(Function)`),
+    ).toBe(true);
+});
+
+test('Should be active on action click', () => {
+    const props = {
+        extension: ExtensionType.JS,
+        name: 'main',
+    };
+
+    const { container } = render(
+        <Provider {...appModels}>
+            <FileButton {...props} />
+        </Provider>,
+    );
+
+    fireEvent.click(screen.getByText('main.js'));
+    expect(container.firstChild).toHaveClass('file-button--active');
 });
